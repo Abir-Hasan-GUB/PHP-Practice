5381288e23e4c3470f97478cba5740ccfd2d883d
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2023-02-06 22:29:28 +0600

    Alhamdulillah Assignment two of ostad done

--- OstadExam/assignment-2.php
+++ OstadExam/assignment-2.php
@@ -2,6 +2,10 @@
 
 $tutionFee = 7001;
 
-$commission = ($tutionFee >= 20000) ? "25%" : (($tutionFee >= 10000 && $tutionFee < 20000) ? "20%" : (($tutionFee > 7000 && $tutionFee < 10000) ? "15%" : (($tutionFee > 7000) ? "10%" : (($tutionFee < 7000) ? 'Invalid' : 'Invalid'))));
+$commission = ($tutionFee >= 20000) ? "25%" :
+(($tutionFee >= 10000 && $tutionFee < 20000) ? "20%" :
+    (($tutionFee > 7000 && $tutionFee < 10000) ? "15%" :
+        (($tutionFee > 7000) ? "10%" :
+            (($tutionFee < 7000) ? 'Invalid' : 'Invalid'))));
 
 echo $commission;
