51e0d8763320d5666c6ef167c1b645ddb9ce226d
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2023-11-18 00:55:48 +0600

    CRUD project intalization done

---
+++ Module-8-CRUD-Project/inc/templates/nav.php
@@ -0,0 +1,15 @@
+<div style="border-bottom: 1px solid; border-color:#eee; padding-bottom: 30px; margin-bottom:30px;">
+    <div class="float-left">
+        <p>
+            <a href="/crud/index.php?task=report">All Students</a>
+                |
+                <a href="/crud/index.php?task=add">Add New Student</a>
+                |
+                <a href="/crud/index.php?task=seed">Seed</a>
+        </p>
+    </div>
+    <div class="float-right">
+            <a href="/crud/auth.php?logout=true">Log Out (<?php echo $_SESSION['role'] ?>)</a>
+    </div>
+    <p></p>
+</div>
